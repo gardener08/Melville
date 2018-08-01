01a37b5d81f6ebc4984dde396f53c4e47cde5e84
Adam Risius <eagle-codeprojects@usa.net>
2018-08-01 11:06:56 -0500

    Fixed the initial count value by making it 1 instead of 0.  Changed the RegEx to exclude numbers.  Added additional debugging statements.  The numbers and already accounted for words are not being added to the superkey list, as expected.

--- MobyDickProject.py
+++ MobyDickProject.py
@@ -8,7 +8,7 @@ for line in mobyDickTextFile:
 	lineForAnalysis = str(line)
 	lineForAnalysisWithoutNewline = lineForAnalysis[:-1]
 	#print(lineForAnalysisWithoutNewline + '###')
-	regEx = r'[\W]'
+	regEx = r'[^a-zA-Z_]'
 	lineForAnalysisWithoutNewlineOrWhitespace = lineForAnalysisWithoutNewline.rstrip()
 	brokenUpLine = re.split(regEx, lineForAnalysisWithoutNewlineOrWhitespace)
 	
@@ -20,8 +20,32 @@ for line in mobyDickTextFile:
 		elif value in wordDictionary:
 			wordDictionary[value] = wordDictionary[value] + 1
 		else:
-			wordDictionary[value] = 0
-		
+			wordDictionary[value] = 1
+
+superKeyList = []
+wordDictionaryKeys = wordDictionary.keys()
+for key in wordDictionaryKeys:
+	superKeyList.append(str(wordDictionary[key]) + ':' + key)
+
 print('Number of words in Moby Dick is: ' + str(len(wordDictionary)))
+print('Number of words in superKeyList is: ' + str(len(superKeyList)))
+print('Example SuperKey: ' + superKeyList[10])
+print('Example SuperKey: ' + superKeyList[20])
+print('Example SuperKey: ' + superKeyList[30])
+print('Example SuperKey: ' + superKeyList[40])
+print('Example SuperKey: ' + superKeyList[41])
+print('Example SuperKey: ' + superKeyList[42])
+print('Example SuperKey: ' + superKeyList[43])
+print('Example SuperKey: ' + superKeyList[44])
+print('Example SuperKey: ' + superKeyList[45])
+print('Example SuperKey: ' + superKeyList[46])
+print('Example SuperKey: ' + superKeyList[47])
+print('Example SuperKey: ' + superKeyList[48])
+print('Example SuperKey: ' + superKeyList[49])
+
+print('Example SuperKey: ' + superKeyList[50])
+print('Example SuperKey: ' + superKeyList[60])
+
+
 
 
