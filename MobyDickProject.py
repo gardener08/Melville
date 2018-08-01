26a36e694c825bd8befb9d8c204b8fc957337843
Adam Risius <eagle-codeprojects@usa.net>
2018-08-01 10:33:03 -0500

    Digested the input file and set up a dictionary of distinct words in the file.  Printed a count of the number of distinct words in the file.

--- MobyDickProject.py
+++ MobyDickProject.py
@@ -1,7 +1,27 @@
 # This is the Moby Dick program
+import re
 mobyDickTextFile = open('MobyDickInputFile.txt',encoding='utf-8')
 lineCount = 0
+wordDictionary = {}
 for line in mobyDickTextFile:
 	lineCount = lineCount + 1
-print(lineCount)
+	lineForAnalysis = str(line)
+	lineForAnalysisWithoutNewline = lineForAnalysis[:-1]
+	#print(lineForAnalysisWithoutNewline + '###')
+	regEx = r'[\W]'
+	lineForAnalysisWithoutNewlineOrWhitespace = lineForAnalysisWithoutNewline.rstrip()
+	brokenUpLine = re.split(regEx, lineForAnalysisWithoutNewlineOrWhitespace)
+	
+	#print('line: ' + lineForAnalysisWithoutNewlineOrWhitespace + '###')
+	#print(brokenUpLine)
+	for value in brokenUpLine:
+		if value == '' or value is None or value is '\n':
+			continue
+		elif value in wordDictionary:
+			wordDictionary[value] = wordDictionary[value] + 1
+		else:
+			wordDictionary[value] = 0
+		
+print('Number of words in Moby Dick is: ' + str(len(wordDictionary)))
+
 
