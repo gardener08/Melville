6b66e52f263395eb346742bfa0018064c73919f3
Adam Risius <eagle-codeprojects@usa.net>
2018-08-02 08:10:27 -0500

    Broke out logic into methods to better enable creation of a GUI component.

--- MobyDickProject.py
+++ MobyDickProject.py
@@ -17,77 +17,76 @@ def readStopWordList():
 			wordsToRemove.append(lineForAnalysisWithoutNewlineOrWhitespace)
 	return wordsToRemove
 
-mobyDickTextFile = open('MobyDickInputFile.txt',encoding='utf-8')
-lineCount = 0
-wordDictionary = {}
+def setupWordDictionary():
+	mobyDickTextFile = open('MobyDickInputFile.txt',encoding='utf-8')
+	lineCount = 0
+	wordDictionary = {}
 
-stopWordList = readStopWordList()
-#print(stopWordList)
+	stopWordList = readStopWordList()
+	#print(stopWordList)
 
-for line in mobyDickTextFile:
-	lineCount = lineCount + 1
-	lineForAnalysis = str(line)
-	lineForAnalysisWithoutNewline = lineForAnalysis[:-1]
-	#print(lineForAnalysisWithoutNewline + '###')
-	regEx = r'[^a-zA-Z_]'
-	lineForAnalysisWithoutNewlineOrWhitespace = lineForAnalysisWithoutNewline.rstrip()
-	brokenUpLine = re.split(regEx, lineForAnalysisWithoutNewlineOrWhitespace)
-	
-	#print('line: ' + lineForAnalysisWithoutNewlineOrWhitespace + '###')
-	#print(brokenUpLine)
-	for value in brokenUpLine:
-		if value == '' or value is None or value is '\n':
-			continue
-		elif value not in stopWordList:
-			if value in wordDictionary:
-				wordDictionary[value] = wordDictionary[value] + 1
-			else:
-				wordDictionary[value] = 1
-
-wordsByCount = {}
-
-# value is the count	
-for wordKey in wordDictionary.keys():
-	# is there a word with this particular count 
-	currentWord = wordKey
-	countOfCurrentWord = wordDictionary[wordKey]
-	if countOfCurrentWord in wordsByCount:
-		listOfWordsWithThisCount = wordsByCount[countOfCurrentWord]
-		listOfWordsWithThisCount.append(currentWord)
-	else:
-		listOfWordsToCreateForThisCount = []
-		listOfWordsToCreateForThisCount.append(currentWord)
-		wordsByCount[countOfCurrentWord] = listOfWordsToCreateForThisCount
+	for line in mobyDickTextFile:
+		lineCount = lineCount + 1
+		lineForAnalysis = str(line)
+		lineForAnalysisWithoutNewline = lineForAnalysis[:-1]
+		#print(lineForAnalysisWithoutNewline + '###')
+		regEx = r'[^a-zA-Z_]'
+		lineForAnalysisWithoutNewlineOrWhitespace = lineForAnalysisWithoutNewline.rstrip()
+		brokenUpLine = re.split(regEx, lineForAnalysisWithoutNewlineOrWhitespace)
 		
-wordCounts = list(wordsByCount.keys())
-wordCounts.sort(reverse=True)
+		#print('line: ' + lineForAnalysisWithoutNewlineOrWhitespace + '###')
+		#print(brokenUpLine)
+		for value in brokenUpLine:
+			if value == '' or value is None or value is '\n':
+				continue
+			elif value not in stopWordList:
+				if value in wordDictionary:
+					wordDictionary[value] = wordDictionary[value] + 1
+				else:
+					wordDictionary[value] = 1
+	return wordDictionary
+				
+def getWordsByCount(wordDictionary):
+		
+	wordsByCount = {}
 
-topTenWordCounts = wordCounts[0:10]
+	# value is the count	
+	for wordKey in wordDictionary.keys():
+		# is there a word with this particular count 
+		currentWord = wordKey
+		countOfCurrentWord = wordDictionary[wordKey]
+		if countOfCurrentWord in wordsByCount:
+			listOfWordsWithThisCount = wordsByCount[countOfCurrentWord]
+			listOfWordsWithThisCount.append(currentWord)
+		else:
+			listOfWordsToCreateForThisCount = []
+			listOfWordsToCreateForThisCount.append(currentWord)
+			wordsByCount[countOfCurrentWord] = listOfWordsToCreateForThisCount
+			
+	return wordsByCount
 
-print('Number of unique words in Moby Dick is: ' + str(len(wordDictionary)))
+def getTopTenWordCounts(wordsByCount):
+	wordCounts = list(wordsByCount.keys())
+	wordCounts.sort(reverse=True)
 
-print('Here are the top ten word counts with their words')
-for wordCount in topTenWordCounts:
-	wordsForThisCount = wordsByCount[wordCount]
-	wordsForThisCountStr = ''
-	for word in wordsForThisCount:
-		wordsForThisCountStr += word + ','
-	# Remove the trailing comma
-	wordsForThisCountStr = wordsForThisCountStr[:-1]
-	print(str(wordCount) + ': ' + wordsForThisCountStr)
-
-stopWordFile = open('StopWords.txt',encoding='utf-8')
-wordsToRemove = []
-for line in stopWordFile:
-	lineForAnalysisWithoutNewline = line[:-1]
-	lineForAnalysisWithoutNewlineOrWhitespace = lineForAnalysisWithoutNewline.rstrip()
-	#lineForAnalysisWithoutWhitespace = lineForAnalysisWithoutNewline.rstrip()
-	#	print(line)
+	topTenWordCounts = wordCounts[0:10]
+	return topTenWordCounts
 	
-	commentLineRegEx = r'^#'
-	if lineForAnalysisWithoutNewlineOrWhitespace is '' or lineForAnalysisWithoutNewlineOrWhitespace is None or lineForAnalysisWithoutNewlineOrWhitespace is '\n':
-		continue
-	elif re.match(commentLineRegEx, lineForAnalysisWithoutNewlineOrWhitespace):
-		continue
-	else:
-		wordsToRemove.append(lineForAnalysisWithoutNewlineOrWhitespace)
+def outputResults(wordDictionary, wordsByCount, topTenWordCounts):
+	
+	print('Number of unique words in Moby Dick is: ' + str(len(wordDictionary)))
+
+	print('Here are the top ten word counts with their words')
+	for wordCount in topTenWordCounts:
+		wordsForThisCount = wordsByCount[wordCount]
+		wordsForThisCountStr = ''
+		for word in wordsForThisCount:
+			wordsForThisCountStr += word + ','
+		# Remove the trailing comma
+		wordsForThisCountStr = wordsForThisCountStr[:-1]
+		print(str(wordCount) + ': ' + wordsForThisCountStr)
+
+wordDictionary = setupWordDictionary()
+wordsByCount = getWordsByCount(wordDictionary)
+topTenWordsByCount = getTopTenWordCounts(wordsByCount)
+outputResults(wordDictionary, wordsByCount, topTenWordsByCount)
